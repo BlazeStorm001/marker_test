438980a814f4e7706decc4c6d94dad58534ec645
Namit Arora <namit0010@gmail.com>
2025-03-31 00:11:40 +0530

    add resume

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from marker.converters.pdf import PdfConverter
 from marker.models import create_model_dict
 from marker.output import text_from_rendered
 from marker.config.parser import ConfigParser
-from pdb import set_trace
 config = {
     "output_format": "json",
     "disable_image_extraction": True
@@ -17,11 +16,9 @@ converter = PdfConverter(
     renderer=config_parser.get_renderer(),
     llm_service=config_parser.get_llm_service()
 )
-print(config_parser.get_processors())
-set_trace()
 import time
 tic = time.time()
-rendered = converter("D:\\Desktop\\fastapi_demo\\resumes\\shivam_kumar_f.pdf")
+rendered = converter("resume.pdf")
 text, _, images = text_from_rendered(rendered)
 toc = time.time()
 print(text)
